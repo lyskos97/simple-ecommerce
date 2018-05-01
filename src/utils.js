e972e00ed3a4bffe2b4895547a9c6dff7f6e1437
Konstantin Lyssenko <lyskos97@gmail.com>
2018-05-01 12:09:30 +0600

    feat(utils): `getUserFromSession`

---
+++ src/utils.js
@@ -0,0 +1,28 @@
+const User = require('./models/user');
+
+module.exports = {
+  middlewares: {
+    authenticate: (req, res, next) => {
+      if (req.session && req.session.userId) next();
+      else {
+        res.render('page404');
+        next(new Error('Not authorized'));
+      }
+    }
+  },
+  getUserFromSession: async req => {
+    if (req.session && req.session.userId) {
+      const user = await User.findById(req.session.userId);
+
+      if (user) {
+        const { lastName, firstName } = user;
+
+        return { lastName, firstName };
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+};
